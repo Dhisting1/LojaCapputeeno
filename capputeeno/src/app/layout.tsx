95d18a24bb456568a501fc4ea3c40fddc08f4f99
Lucas Fernando Gonlçaves Lima <lcs.lima0708@gmail.com>
2023-10-30 14:50:08 -0300

    refatorando

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,9 +1,9 @@
+
 import Header from '@/components/header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Saira } from 'next/font/google'
-import { FilterContextProvider } from '@/context/filter-context'
-
+import { DefaultProviders } from '@/components/default-providers'
 
 const saira = Saira({
   weight: ["300", "400", "500", "600"],
@@ -23,10 +23,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={saira.className}>
-        <FilterContextProvider>
+        <DefaultProviders>
           <Header />
           {children}
-        </FilterContextProvider>
+        </DefaultProviders>
       </body>
     </html>
   )
